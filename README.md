20366e3be5f1093fb8b41480b28eb6ac7a2f8d09
Kirill Valyushko <valyushko.kirill@gmail.com>
2021-06-24 16:06:11 +0300

    add demo with clustered 3 master and 3 replica nodes

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-```
-docker exec -it redis_1 redis-cli --cluster create 10.1.0.7:6379 10.1.0.5:6379 10.1.0.6:6379 10.1.0.3:6379 10.1.0.4:6379 10.1.0.2:6379 --cluster-replicas 1
-```
+1. Build application jar
+2. Run docker compose
--- src/main/java/co/altir/config/RedisKeyspaceConfig.java
+++ src/main/java/co/altir/config/RedisKeyspaceConfig.java
@@ -1,19 +1,30 @@
 package co.altir.config;
 
 import co.altir.config.RedisKeyspaceConfig.EnvironmentKeyspaceConfiguration;
+import co.altir.model.Record;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.convert.KeyspaceConfiguration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableRedisRepositories(keyspaceConfiguration = EnvironmentKeyspaceConfiguration.class)
 public class RedisKeyspaceConfig {
 
-    public static class EnvironmentKeyspaceConfiguration extends KeyspaceConfiguration {
+    @Value("${app.redis-keyspace}")
+    private String redisKeyspace;
+
+    public class EnvironmentKeyspaceConfiguration extends KeyspaceConfiguration {
 
         @Override
         protected Iterable<KeyspaceSettings> initialConfiguration() {
-            return super.initialConfiguration();
+            List<KeyspaceSettings> settings = new ArrayList<>();
+            settings.add(new KeyspaceSettings(Record.class, redisKeyspace + "." + Record.class.getName()));
+
+            return settings;
         }
 
     }
